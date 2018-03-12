822c13bdfccb277ef10ab2914b5fadc236918ae2
agatakrawczyk <agata.lucja.krawczyk@gmail.com>
2018-03-12 19:55:52 +0100

    Create orders table. Change associations in Order and Product class.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,3 @@
 class Product < ApplicationRecord
-  belongs_to :order
-
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306153218) do
+ActiveRecord::Schema.define(version: 20180312184800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
@@ -39,4 +44,5 @@ ActiveRecord::Schema.define(version: 20180306153218) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
