bd12a2dd03af60e1ebce4c8fa9b5609d1fafa8ce
agatakrawczyk <agata.lucja.krawczyk@gmail.com>
2018-03-13 02:28:29 +0100

    Add column price to products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312185824) do
+ActiveRecord::Schema.define(version: 20180313011224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180312185824) do
     t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "price", default: "0.0", null: false
   end
 
   create_table "users", force: :cascade do |t|
