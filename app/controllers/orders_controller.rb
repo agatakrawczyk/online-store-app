016aa509aca04adb03af9f0168e3064c9a3fad3c
agatakrawczyk <agata.lucja.krawczyk@gmail.com>
2018-03-13 22:19:05 +0100

    Change show action in Order controller by specifying orders of current user.
    Add index, new, edit actions in Order controller.
    Add price to strong parameters of Product controller.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @orders = current_user.orders.all
+  end
 
   def show
-    @order = Order.find(params[:id])
+    @order = current_user.orders.find(params[:id])
+  end
+
+  def new
+    @order = current_user.orders.new
+  end
+
+  def edit
+    @order = current_user.orders.find(params[:id])
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,7 +48,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description)
+    params.require(:product).permit(:name, :description, :price)
   end
 
 end
