50159cceb92172f14615e2185075960b9f69c3b0
agatakrawczyk <agata.lucja.krawczyk@gmail.com>
2018-10-05 00:09:23 +0200

    Update product controller methods, models validations and view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_admin!, only: [:edit, :new]
 
   def index
     @products = Product.all
@@ -51,7 +52,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price)
+    params.require(:product).permit(:name, :description, :price, :image)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
-  has_many :line_items
-
+  has_many :line_items 
   validates :name, presence: true
+  
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
 end
