220c91968f1e568ea35a66efe6d5be31a67a2b7e
agatakrawczyk <agata.lucja.krawczyk@gmail.com>
2018-12-04 16:13:12 +0100

    Add quantity column to line items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181004230153) do
+ActiveRecord::Schema.define(version: 20181201163811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20181004230153) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "cart_id"
+    t.integer "quantity", default: 1, null: false
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
