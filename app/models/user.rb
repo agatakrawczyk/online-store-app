a6a2aefe400b25724073ed6615faea35a40747de
agatakrawczyk <agata.lucja.krawczyk@gmail.com>
2019-10-29 21:01:08 +0100

    Add user email and password validations in user model & add device error messages in login signup forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
   has_many :carts
   has_one :cart
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates :email, :password, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+
 end
