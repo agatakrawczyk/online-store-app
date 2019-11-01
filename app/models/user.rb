8bd6cbe512ed7878aea45d5e53531b97cc9893a1
agatakrawczyk <agata.lucja.krawczyk@gmail.com>
2019-11-01 22:33:51 +0100

    Add validations and error messages to sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
   validates :email, :password, presence: true
   validates :email, uniqueness: true
   validates :password, length: { minimum: 6 }
